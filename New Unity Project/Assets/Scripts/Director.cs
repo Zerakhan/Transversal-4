3f6b2716e4c24d60db61ddc887970457cf1cc4f6
Zerakhan <Keymaster.1.13.2@gmail.com>
2018-10-31 00:02:24 +0100

    Alpha finalizada

--- New Unity Project/Assets/Scripts/Director.cs
+++ New Unity Project/Assets/Scripts/Director.cs
@@ -3,35 +3,21 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Director : MonoBehaviour {
-
-    public GameObject[] NPC;
+    Pregunta pregunta;
     public int Preguntado = 0;
-    public int purga = 1;
     public GameObject Prego;
     public GameObject Brego;
-    public GameObject Trego;
-    public GameObject Grego;
     public GameObject Frego;
-    public GameObject Rrego;
-    public bool Raro = false;
-    public bool Rara = false;
-    public bool Roro = false;
-    public bool Rora = false;
-    public int Zeras = 0;
-    public static int zara = 1;
-    public static int zare = 2;
-    public static int zere = 3;
-    public static int zera = 4;
-    public int Buena = 0;
+    public GameObject Trego;
+    public GameObject Lego;
 
     void Start()
     {
-        NPC = GameObject.FindGameObjectsWithTag("Enemigo");
-        Grego.SetActive(false);
+        Trego.SetActive(false);
         Prego.SetActive(false);
         Brego.SetActive(false);
-        Trego.SetActive(false);
         Frego.SetActive(false);
+        Lego.SetActive(false);
     }
 
     void Update()
@@ -43,76 +29,26 @@ public class Director : MonoBehaviour {
         else if (Preguntado == 1)
         {
             Frego.SetActive(false);
-
-            if (!Raro)
-            {
-            Prego.SetActive(true);
-            }
-            else
-            {
-            Prego.SetActive(false);
-            }
-
-            if (!Rora)
-            {
             Brego.SetActive(true);
-            }
-            else
-            {
-            Brego.SetActive(false);
-            }
-
-            if (!Roro) {
-            Grego.SetActive(true);
-            }
-            else
-            {
-            Grego.SetActive(false);
-            }
-
-            if (!Rara) {
-            Trego.SetActive(true);
-            }
-            else
-            {
-            Trego.SetActive(false);
-            }
         }
         else if (Preguntado == 2)
         {
-            Prego.SetActive(false);
             Brego.SetActive(false);
-            Grego.SetActive(false);
-            Trego.SetActive(false);
-            Rrego.SetActive(true);
+            Prego.SetActive(true);
         }
-
-        if (Preguntado == 14)
+        else if (Preguntado == 3)
         {
-            Frego.SetActive(false);
             Prego.SetActive(false);
-            Brego.SetActive(false);
-            Grego.SetActive(false);
-            Trego.SetActive(false);
-            Rrego.SetActive(false);
+            Trego.SetActive(true);
+            Lego.SetActive(true);
         }
     }
     public void Clicked()
     {
-        Preguntado += purga;
+        Preguntado = Preguntado + 1;
     }
-    public void FinOrdenes(Pregunta pregunta, bool Vaya)
+    public void FinOrdenes()
     {
-        Preguntado = 1;
-        Raro = Vaya;
-        Zeras++;
-        if(Zeras == zara)
-        {
-            Buena++;
-        }
-    }
-    public void VayaToalla(Pregunta pregunta, bool Vaya)
-    {
-        Preguntado = 14;
+        this.Preguntado = 1;
     }
 }
--- New Unity Project/Assets/Scripts/Pregunta.cs
+++ New Unity Project/Assets/Scripts/Pregunta.cs
@@ -3,42 +3,32 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Pregunta : MonoBehaviour {
-    Director dir;
-    public int Preguntado = 0;
-    public int purga = 1;
+
+    public int Preguntado = 1;
     public GameObject Prego;
     public GameObject Brego;
-    public bool Vaya = true;
+    public GameObject Trego;
 
     void Update () {
         if (Preguntado == 1)
         {
-            VayaToalla();
-            Prego.SetActive(false);
-            Brego.SetActive(true);
-
+            Brego.SetActive(false);
         }
         else if (Preguntado == 2)
-        { 
-            Brego.SetActive(false);
-            FinOrden();
+        {
+            Prego.SetActive(false);
+            Brego.SetActive(true);
+            Trego.SetActive(true);
         }
 
     }
     public void Clicked()
     {
-        Preguntado += purga;
+        Preguntado = Preguntado + 1;
     }
-    public void VayaToalla()
-    {
-        dir.VayaToalla(this, Vaya);
-    }
-    public void FinOrden()
-    {
-        dir.FinOrdenes(this, Vaya);
-    }
+
     public void Infografía()
     {
-        Preguntado++;
+        this.Preguntado = 1;
     }
 }
