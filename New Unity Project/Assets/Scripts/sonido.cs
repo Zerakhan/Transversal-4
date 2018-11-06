9324b58d32f6c3273b13f07cbda9423c0f47a9d0
ihateblonde <32159981+ihateblonde@users.noreply.github.com>
2018-11-06 21:47:54 +0100

    aa

--- New Unity Project/Assets/Scripts/sonido.cs
+++ New Unity Project/Assets/Scripts/sonido.cs
@@ -6,16 +6,23 @@ public class sonido : MonoBehaviour {
 
     private AudioSource audioSrc;
     private float musicVolume = 1f;
+    float m_MySliderValue;
+    AudioSource m_MyAudioSource;
 
     void Start()
     {
         audioSrc = GetComponent<AudioSource>();
+        m_MySliderValue = 0.5f;
+        m_MyAudioSource = GetComponent<AudioSource>();
+        m_MyAudioSource.Play();
     }
 
     void Update()
     {
         audioSrc.volume = musicVolume;
     }
+
+   
 
     public void SetVolume(float vol)
     {
