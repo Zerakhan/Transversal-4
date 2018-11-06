6f9a0af2bb85859f62df7e413649869b420bffa7
Zerakhan <Keymaster.1.13.2@gmail.com>
2018-11-06 22:20:30 +0100

    Solventando mierda

--- New Unity Project/Assets/Scripts/sonido.cs
+++ New Unity Project/Assets/Scripts/sonido.cs
@@ -4,20 +4,12 @@ using UnityEngine;
 
 public class sonido : MonoBehaviour {
 
-    private AudioSource audioSrc;
-    private float musicVolume = 1f;
-<<<<<<< HEAD
     float m_MySliderValue;
     AudioSource m_MyAudioSource;
-<<<<<<< HEAD
-=======
+
     public AudioSource audioSrc;
     public float musicVolume = 0.5f;
->>>>>>> 0f044b768662aa142987bfcbd1dc3841ab41b3f3
-=======
->>>>>>> parent of 9324b58... aa
-=======
->>>>>>> parent of f1e54a6... Merge branch 'master' of https://github.com/Zerakhan/Transversal-4
+
 
     void Start()
     {
