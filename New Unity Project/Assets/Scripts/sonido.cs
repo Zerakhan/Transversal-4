0ae2625b2fe583182a6fb26c73640e2553617c1c
Zerakhan <Keymaster.1.13.2@gmail.com>
2018-11-06 22:15:01 +0100

    Revert "aa"
    This reverts commit 9324b58d32f6c3273b13f07cbda9423c0f47a9d0.

--- New Unity Project/Assets/Scripts/sonido.cs
+++ New Unity Project/Assets/Scripts/sonido.cs
@@ -7,27 +7,25 @@ public class sonido : MonoBehaviour {
 <<<<<<< HEAD
     private AudioSource audioSrc;
     private float musicVolume = 1f;
+<<<<<<< HEAD
     float m_MySliderValue;
     AudioSource m_MyAudioSource;
 =======
     public AudioSource audioSrc;
     public float musicVolume = 0.5f;
 >>>>>>> 0f044b768662aa142987bfcbd1dc3841ab41b3f3
+=======
+>>>>>>> parent of 9324b58... aa
 
     void Start()
     {
         audioSrc = GetComponent<AudioSource>();
-        m_MySliderValue = 0.5f;
-        m_MyAudioSource = GetComponent<AudioSource>();
-        m_MyAudioSource.Play();
     }
 
     void Update()
     {
         audioSrc.volume = musicVolume;
     }
-
-   
 
     public void SetVolume(float vol)
     {
